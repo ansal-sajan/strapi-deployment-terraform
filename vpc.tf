541f82fc45a1b2f006ff5b7ff3dedd6311185615
Ansal-sajan <ansalsajan.eee@gmail.com>
2024-07-07 11:59:27 +0000

    Edited

--- vpc.tf
+++ vpc.tf
@@ -48,33 +48,3 @@ resource "aws_route_table_association" "public_association" {
   subnet_id      = element(aws_subnet.public[*].id, count.index)
   route_table_id = aws_route_table.public.id
 }
-
-resource "aws_security_group" "ecs_sg" {
-  name_prefix = "ecs-sg-"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 1337
-    to_port     = 1337
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ecs-sg"
-  }
-}
